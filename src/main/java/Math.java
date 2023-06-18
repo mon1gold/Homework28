51c6c0bf26f1363a294e21a7afd3eb0038caee4d
mon1gold <daniildocenko218@gmail.com>
2023-06-18 17:49:00 +0300

    add homework

---
+++ src/main/java/Math.java
@@ -0,0 +1,17 @@
+public class Math {
+    public static double multiply(double x, double y) {
+        return x * y;
+    }
+
+    public static double sum(double x, double y) {
+        return x + y;
+    }
+
+    public static double subtraction(double x, double y) {
+        return x - y;
+    }
+
+    public static double divide(double x, double y) {
+        return x / y;
+    }
+}
